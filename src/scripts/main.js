256e3260115c926e9a5918afc11cecf4187c8f67
Sinver Aguilo <sinveraguilo@gmail.com>
2018-07-04 23:36:11 -0400

    first commit remake

---
+++ src/scripts/main.js
@@ -0,0 +1,79 @@
+// const canvas = document.getElementById('tetris');
+// const context = canvas.getContext('2d');
+// const figures = [
+//     [
+//         [0, 1, 0, 0],
+//         [0, 1, 0, 0],
+//         [0, 1, 0, 0],
+//         [0, 1, 0, 0],
+//     ],
+//     [
+//         [0, 1, 0],
+//         [0, 1, 0],
+//         [0, 1, 1]
+//     ],
+//     [
+//         [0, 1, 0],
+//         [0, 1, 0],
+//         [1, 1, 0],
+//     ],
+//     [
+//         [1, 1],
+//         [1, 1],
+//     ],
+//     [
+//         [1, 1, 0],
+//         [0, 1, 1],
+//         [0, 0, 0],
+//     ],
+//     [
+//         [0, 1, 1],
+//         [1, 1, 0],
+//         [0, 0, 0],
+//     ],
+//     [
+//         [0, 1, 0],
+//         [1, 1, 1],
+//         [0, 0, 0],
+//     ]
+// ];
+// const colors = [
+//     '#FF0D72',
+//     '#0DC2FF',
+//     '#0DFF72',
+//     '#F538FF',
+//     '#FF8E0D',
+//     '#FFE138',
+//     '#3877FF',
+// ];
+// context.scale(20, 20);
+
+
+
+// // function clear() {
+// //     context.fillStyle = '#000';
+// //     context.fillRect(0, 0, canvas.width, canvas.height);
+// // }
+
+// // let dropCounter = 0;
+// // let dropInterval = 1000;
+// // let lastTime = 0;
+// // document.addEventListener('keydown', (event) => {
+// //     // if(event.keyCode === 37) {
+// //     //     playerMove(-1);
+// //     // } else if(event.keyCode === 39) {
+// //     //     playerMove(1);
+// //     // } else if(event.keyCode === 40) {
+// //     //     playerDrop();
+// //     // } else if (event.keyCode === 38) {
+// //     //     playerRotate(1);
+// //     // }
+// // });
+
+// // const arena = createMatrix(12, 20);
+
+// // const player = {
+// //     position: {x: 0, y: 0},
+// //     shape: null,
+// //     score: 0,
+// // };
